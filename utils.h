1746fe4166f9dca9d9c089b7c2fd456777854839
thanhtin.nguyen <thanhtinqn0903039105@gmail.com>
2023-01-29 16:15:53 -0600

    Create utils.h
    project2

---
+++ utils.h
@@ -0,0 +1,15 @@
+/*
+ * COMP 7500: Project 2: pWordCount: A Pipe-based WordCount Tool 
+ * Thanh Tin Nguyen - 904285164 
+ * ttn0011@auburn.edu
+ * Date:
+ * Auburn University
+*/
+
+/*
+ * This is the header file of utils.c, contains the initialization of functions written in utils.c
+*/
+
+int count_words(char *word);
+char* check_filename_extension(char *filename);
+void error_checking(int error_type);
