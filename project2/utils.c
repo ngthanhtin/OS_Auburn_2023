f86ebf0e0a6addad6e50f8017f80fed82967d19e
thanhtin.nguyen <thanhtinqn0903039105@gmail.com>
2023-02-09 00:15:18 -0600

    Update utils.c

--- project2/utils.c
+++ project2/utils.c
@@ -1,7 +1,7 @@
 /* COMP 7500: Project 2: pWordCount: A Pipe-based WordCount Tool 
  * Thanh Tin Nguyen - 904285164 
  * ttn0011@auburn.edu
- * Date:
+ * Date: Date: 2/9/2023
  * Auburn University
  */
 #include<stdio.h>
@@ -21,12 +21,12 @@ int count_words(char *word)
         int num_words = 0; // this var is to store the number of words
         int len = strlen(word); // get the length of string word
         
-        int pos; // this var shows the position of the pointer traversing the string word one by one
+        int pos; // this var shows the position of the pointer in the string
         bool pointer_still_in_a_word = false; // this var is to tell if the current position of the pointer 
                 // is still pointing to a character except 
                 // '\0' (null), ' ' (space), '\t' (tab), '\n' (new line), '\r' (return)
 
-	for(pos = 0; pos < len; pos++) // the pointer starts traversing the string word from the beginning
+	for(pos = 0; pos < len; pos++) // the pointer starts traversing the string from the beginning
         {
                 char cur_char = word[pos]; // this var is to store the current character
                 if(cur_char == '\0' || cur_char == ' ' || cur_char == '\t' || cur_char == '\n' || cur_char == '\r')
@@ -48,6 +48,12 @@ int count_words(char *word)
 
 char* check_filename_extension(char *filename)
 {
+        //This function will return empty string denoted that if there is no filename extension
+        //It will return the file name extension if there is a filename extension
+
+        //Note that: If "." is the first character in filename. Ex: ".abcdef", 
+        //then, this file is a hidden file, and the filename extension will be "abcdef"
+
         char *dot = strrchr(filename, '.'); // search for the last occurrence of the character "."
         int len = strlen(filename); // get the length of filename
         
@@ -56,18 +62,13 @@ char* check_filename_extension(char *filename)
                 printf("Can not find the \".\" in filename.\n");
                 return "";
         }
-        // else if(strcmp(dot, filename) == 0) // "." is the first character in filename. Ex: ".abcdef"
-        // {       
-        //         printf("\".\" is the first character in filename\n");
-        //         return "";
-        // }
         else if('.' == filename[len-1]) // "." is the last character in filename. Ex: "abcdef."
         {
                 printf("\".\" is the last character in filename\n");
                 return "";
         }
 
-        return dot + 1; // get the string (filename's extension)
+        return dot + 1; // get the string (filename extension)
 }
 
 void error_checking(int error_type, char *additional_msg)
