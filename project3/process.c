76b189412124d78c516a41b033508e1594883e19
thanhtin.nguyen <thanhtinqn0903039105@gmail.com>
2023-03-18 16:50:33 -0500

    Create process.c

---
+++ project3/process.c
@@ -0,0 +1,37 @@
+/* ------------------------------------------------- */
+
+/* Project 3 - AUbatch - A batch scheduling system
+/* Thanh-Tin Nguyen
+/* 18/03/2023
+
+/* -------------------------------------------------- */
+
+/*
+ * This program will be launched in fork_execv
+
+ */
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+  int i,arg1,arg2;
+
+  printf("A process is running ...\n");
+
+
+  if (argc >1 ){
+
+
+  	for(i=0;i<10;i++){
+  		for(i=0;i<50;i++){
+        // printf ("Argument %d is: %s\n", i, argv[i]);
+        // i++;
+        // argc--;
+
+		}
+		}
+		sleep(2);
+ return 0;
+}
+}
+
