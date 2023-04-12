4f273c54befd222da80271ddf903698fd8bb2483
thanhtin.nguyen <thanhtinqn0903039105@gmail.com>
2023-04-12 14:56:12 -0500

    Update cpmfsys.c

--- project4/cpmfsys.c
+++ project4/cpmfsys.c
@@ -1,173 +1,237 @@
 #include "cpmfsys.h"
 #include "diskSimulator.h"
-#include<string.h>
+#include <string.h>
+#include <stdint.h> 
+#include <stdlib.h> 
+#include  <stdbool.h> 
+#include <stdio.h> 
 
+// define constant variables
+#define EXTENT_SIZE 32
+#define BLOCKS_PER_EXTENT 16 
+// define global variables
 bool free_list[256];
 
 /*makes a directory structure containing the file elements from the main memory.*/
 /* Pointer *e was explained in the class taken from Dr. Qin's code. */
 
-DirStructType *mkDirStruct(int index,uint8_t *e){
-
-	DirStructType *d;
+DirStructType *mkDirStruct(int index,uint8_t *e) 
+{
 	/* using malloc to set up the block size was recommeded by Dr. Qin. */
-	d = malloc(sizeof(DirStructType));
-	int i;
-	int j=0;
-		(d -> status) = (e+index*EXTENT_SIZE)[0];
+	DirStructType *d = malloc(sizeof(d));
+	//status
+	uint8_t *loc = (e+index*EXTENT_SIZE);
+	d -> status = loc[0];
+
+	int i = 0;
+	int j = 1;//start byte index of filename
+	//name
+	while (j <= 9)
+	{
+		if (loc[j] != ' ')
+			d -> name[i] = loc[j];
+		i++;
 		j++;
-		i=0;
+	}
+	//add terminator to filename
+	d -> name[8] = '\0';
+	
+	//extension
+	i = 0;
+	j = 9; // start byte index of extension
+	while(j <= 12)
+	{
+		if (loc[j] != ' ')
+			d -> extension[i] = loc[j];
+		i++;
+		j++;
+	}
+	//add teminator to extension
+	d -> extension[3] = '\0';
+	
+	//4 bytes
+	d -> XL = loc[12];
+	d -> BC = loc[13];
+	d -> XH = loc[14]; 
+	d -> RC = loc[15];
 
-		while(j<9){
-			if ((e+index*EXTENT_SIZE)[j] != ' '){	
-				(d -> name)[i] = (e+index*EXTENT_SIZE)[j];
-			}
-			else {
-				(d -> name)[i] = '\0';
-				break;
-			}
-			i++;
-			j++;
-		}
-		i=0; 
-		j=9;
+	//block
+	i = 0;
+	j = 16; // start byte index of blocks
+	for(i = 0; i < BLOCKS_PER_EXTENT; i++) 
+	{
+		d -> blocks[i] = loc[j];
+		j++;
+	}
+	
+	return d;
+}	
 
-		while(j<12){
-			if ((e+index*EXTENT_SIZE)[j] != ' '){	
-				d -> extension[i] = (e+index*EXTENT_SIZE)[j];
-			}
-			else {
-				d -> extension[i] = '\0';
-				break;
-			}
-			j++;
-			i++;
-		}
-		d -> XL = (e+index*EXTENT_SIZE)[12];
-		d -> BC = (e+index*EXTENT_SIZE)[13];
-		d -> XH = (e+index*EXTENT_SIZE)[14];
-		d -> RC = (e+index*EXTENT_SIZE)[15];
+/*gives the directory structure with the file length for each file*/
 
-		j=16;
-		i=0;
-		while(j<32){
-			d -> blocks[j] = (e+index*EXTENT_SIZE)[j];
-			i++;
-			j++;
+void cpmDir()
+{
+	int i, block_i;
+	int block_num = 0;
+	int file_len = 0;
+	uint8_t *block0 = malloc(1024);
+	blockRead(block0, 0);
+	
+	printf("DIRECTORY LISTING\n");
+
+	for(i=0; i<32; i++)
+	{
+		DirStructType *d = mkDirStruct(i, block0);
+		if(d -> status != 0xe5)
+		{
+			block_num = 0;
+			for(block_i = 0; block_i < BLOCKS_PER_EXTENT; block_i++)
+			{
+				if (d -> blocks[block_i]!=0)
+					block_num++;
+			}
+			file_len = (block_num -1)*BLOCK_SIZE + d -> RC *128 + d -> BC;
+
+			fprintf(stdout,"%s.%s %d\n",d->name, d->extension,file_len);
 		}
-		return d;	
+	}
 }
 
 /*writes the directory structure into the main memory*/
-void writeDirStruct(DirStructType *d,uint8_t index, uint8_t *e){	
+void writeDirStruct(DirStructType *d,uint8_t index, uint8_t *e)
+{	
 	int i=0;
 	int j;
-	(e+index*EXTENT_SIZE)[0] = d -> status;
+	uint8_t *loc = (e+index*EXTENT_SIZE);
+	loc[0] = d -> status;
 	i++;
 	j=0;
-	while(i<9){
-		if (d -> name[j] != '\0'){	
-			(e+index*EXTENT_SIZE)[i] = d -> name[j];
+	while(i < 9)
+	{
+		if (d -> name[j] != '\0')
+		{	
+			loc[i] = d -> name[j];
 		}
-		else {
-			(e+index*EXTENT_SIZE)[i] = ' ';
+		else 
+		{
+			loc[i] = ' ';
 		}
 	    j++;
 		i++;
 	}
 	j=0;
-	while(i<12){
-		if (d -> extension[j] != '\0'){	
-			(e+index*EXTENT_SIZE)[i] = d -> extension[j];
+	while(i<12)
+	{
+		if (d -> extension[j] != '\0')
+		{	
+			loc[i] = d -> extension[j];
 		}
-		else {
-			(e+index*EXTENT_SIZE)[i] = ' ';
+		else 
+		{
+			loc[i] = ' ';
 		}
 	    j++;
 		i++;
 	}
-	(e+index*EXTENT_SIZE)[12] = d -> XL;
-	(e+index*EXTENT_SIZE)[13] = d -> BC;
-	(e+index*EXTENT_SIZE)[14] = d -> XH;
-	(e+index*EXTENT_SIZE)[15] = d -> RC;
+	loc[12] = d -> XL;
+	loc[13] = d -> BC;
+	loc[14] = d -> XH;
+	loc[15] = d -> RC;
 	i=16;
 	j=0;
-	while(i<32){
-		(e+index*EXTENT_SIZE)[i] = d -> blocks[j];
+	while(i<32)
+	{
+		loc[i] = d -> blocks[j];
 		i++;
 		j++;
 	}	
 }
 
 /*global free list array that contains a list of used and unused blocks*/
-void makeFreeList(){
+// populate the FreeList global data structure. freeList[i] == true means 
+// that block i of the disk is free. block zero is never free, since it holds
+// the directory. freeList[i] == false means the block is in use. 
+void makeFreeList()
+{
 
-	uint8_t buffer[1024];
-	int index,b_index;
-	DirStructType *cpm_dir;	
+	uint8_t *block0 = malloc(1024);
+	int i, block_i;
+	
+	free_list[0] = false;
 
-	blockRead(buffer,(uint8_t)0);
-
-	for(index;index<256;index++){
-		free_list[index] = true;
+	for(i = 1;i < 256; i++)
+	{
+		free_list[i] = true;
 	}
 
-	for(index=0;index<32;index++){
-		cpm_dir = mkDirStruct(index,buffer);
-		for(b_index = 0;b_index<16;b_index++){
-			if (cpm_dir -> blocks[b_index] == 0)
-				free_list[(int)cpm_dir -> blocks[b_index]] = true;
-			else
-				free_list[(int)cpm_dir -> blocks[b_index]] = false;	
+	// extent
+	blockRead(block0, 0);
+
+	for(i = 0;i < 32; i++)
+	{
+		DirStructType *extent = mkDirStruct(i, block0);
+		if (extent -> status != 0xe5)
+		{
+			for(block_i = 0;block_i < BLOCKS_PER_EXTENT; block_i++)
+			{
+					if (extent -> blocks[block_i] != 0)
+						free_list[(int)extent -> blocks[block_i]] = false;	
+			}
 		}
 	}
-	free_list[0] = false;
-	
 }
 
-//free dot
 /*prints the freelist */
-
-void printFreeList(){
-	int index;
-	char addr[16][3] = {"0","10","20","30","40","50","60","70","80",
-	"90","a0","b0","c0","d0","e0","f0"};
-	int j=0;
+void printFreeList()
+{
 	int i=0;	
+	int j=0;
 	printf("FREE BLOCK LIST: (* means in-use)\n");
-	fprintf(stdout,"%s ",addr[0]);	
+	fprintf(stdout,"%x0: ",0);	
 	i++;
-	free_list[0]=false;
-	while(j>=0 && j<256){
-		if (j%16 == 0 && j!=0){
+	free_list[0] = false;
+	while(j>=0 && j<256)
+	{
+		if (j%16 == 0 && j!=0)
+		{
 			fprintf(stdout,"\n");
-			fprintf(stdout,"%s ",addr[i]);
+			fprintf(stdout,"%x0: ",i);
 			i++;
 		}
-		if (free_list[j]){
+		if (free_list[j] == true)
+		{
 			fprintf(stdout,". ");
 		}
-		else{
+		else
+		{
 			fprintf(stdout,"* ");
 		}
 		j++;
 		
 	}
-	  fprintf(stdout,"\n");
+	fprintf(stdout,"\n");
 }
 
-/*checks if the file name is valid or not*/
-bool checkLegalName(char *name){
+/*
+Checks if the file name is valid or not, a file name is valid if:
+no blank, no special characters, no punctuation, no controls, etc
+*/
+bool checkLegalName(char *name)
+{
 	int i=0;
 	int length=0;
 	int ext_bit;
 	length=strlen(name);
-	if (name[0]!= ' ' || name[0] != '.'|| name[0] != '\0'){		
-		while (name[i]!= '.' && i<8 && i<length){
-			if ((name[i]>=65 && name[i]<=90) || (name[i] >=97 && name[i] <= 122) || (name[i] >=48 && name[i] <= 57) && i<8){
+	if (name[0]!= ' ' || name[0] != '.'|| name[0] != '\0')
+	{		
+		while (name[i]!= '.' && i<8 && i<length)
+		{
+			if ((name[i]>=65 && name[i]<=90) || (name[i] >=97 && name[i] <= 122) || (name[i] >=48 && name[i] <= 57) && i<8)
+			{
 				i++;			
 			}
-			else {
+			else 
+			{
 				
 				return false;
 			}
@@ -178,34 +242,37 @@ bool checkLegalName(char *name){
 			i++;
 			while(ext_bit<3 && i<12 && i<length)
 			{				
-				if ((name[i]>=65 && name[i]<=90) || (name[i] >=97 && name[i] <= 122) || (name[i] >=48 && name[i] <= 57) && (ext_bit<3)){
+				if ((name[i]>=65 && name[i]<=90) || (name[i] >=97 && name[i] <= 122) || (name[i] >=48 && name[i] <= 57) && (ext_bit<3))
+				{
 					
 					i++;
 					ext_bit++;				    
 				}
-				else {
-					
+				else 
+				{
 					return false;
 				}
 			}
 			
 			return true;
 		}
-		else {
+		else 
+		{
 			
 			return false;
 		}
 	
-	return true;
+		return true;
 	}
-	else{
-		
+	else
+	{
 		return false;
 	}
 }
 
 /*returns the index value of the file for which the data is to be accessed.*/
-int findExtentWithName(char *name, uint8_t *block0){
+int findExtentWithName(char *name, uint8_t *block0)
+{
 	bool result;
 	int index;
 	int i=0;
@@ -217,24 +284,32 @@ int findExtentWithName(char *name, uint8_t *block0){
 	int length=0;
 	length=strlen(name);
 	result = checkLegalName(name);
-	if (result == true){
-		while(i>=0 && i<length){
-			if (name[i]!='.'){
+	// Add '\0' to the end of filename and file extension name
+	if (result == true)
+	{
+		while(i>=0 && i<length)
+		{
+			if (name[i]!='.')
+			{
 				sp_name[j]=name[i];
 				j++;
 				i++;			
 			}
-			else if (name[i] == '.'){
+			else if (name[i] == '.')
+			{
 				i++;
 				sp_name[j]='\0';
 				j=0;
-				while(i>0 && i<length){
-					if (name[i]!=' '){
+				while(i>0 && i<length)
+				{
+					if (name[i]!=' ')
+					{
 						sp_ext[j]=name[i];
 						j++;
 						i++;
 					}
-					else if (name[i]==' '){
+					else if (name[i]==' ')
+					{
 						sp_ext[j]='\0';
 						break;
 					}
@@ -243,54 +318,32 @@ int findExtentWithName(char *name, uint8_t *block0){
 				sp_ext[j]='\0';
 			}
 		}
-		for(index=0;index<32;index++){
-			cpm_dir = mkDirStruct(index,block0);
-				if (((strcmp(sp_name,cpm_dir->name))==0) && ((strcmp(sp_ext,cpm_dir->extension))==0)){
-					if(cpm_dir -> status != 0xe5){
-						return index;
-					}
-					else
-						return -1;
-				}			
-			}
-	}	
-		else 
-			return false;
-	}
-
-/*gives the directory structure with the file length for each file*/
-
-void cpmDir(){
-
-	int index, b_index;
-	int b_num;
-	int filelen = 0;
-	uint8_t buffer[1024];
-	DirStructType *cpm_dir;	
-
-	blockRead(buffer,(uint8_t)0);
-
-	printf("DIRECTORY LISTING\n");
-
-	for(index=0; index<32; index++){
-		cpm_dir = mkDirStruct(index,buffer);
-		if(cpm_dir -> status != 0xe5)
+		for(index=0;index<32;index++)
 		{
-			b_num = 0;
-			for(b_index = 0; b_index<16; b_index++){
-
-				if (cpm_dir -> blocks[b_index]!=0)
-					b_num++;
-			}
-			filelen = (b_num -1)*1024+(cpm_dir -> RC)*128+(cpm_dir -> BC);
-
-			fprintf(stdout,"%s.%s %d\n",cpm_dir->name,cpm_dir->extension,filelen);
+			cpm_dir = mkDirStruct(index,block0);
+			if (((strcmp(sp_name,cpm_dir->name))==0) && ((strcmp(sp_ext,cpm_dir->extension))==0))
+			{
+				if(cpm_dir -> status != 0xe5)
+				{
+					return index;
+				}
+			}			
 		}
+		return -1;
+	}	
+	else 
+	{
+		printf("This file name is not valid.\n");
+		return false;
 	}
+		
 }
 
+
+
 /*	delete a filename and its allocated blocks with the filename passed as a parameter*/
-int  cpmDelete(char * name){
+int  cpmDelete(char * name)
+{
 
 	uint8_t buffer[1024];
 	DirStructType *cpm_dir;
@@ -301,27 +354,31 @@ int  cpmDelete(char * name){
 
 	result= findExtentWithName(name,buffer);
 
-	if (result!=-1){		
+	if (result!=-1)
+	{		
 	    cpm_dir = mkDirStruct(result,buffer);
 		cpm_dir -> status = 0xe5;
 		index=0;
-		while(index<16){		
+		while(index<16)
+		{		
 			free_list[(int)cpm_dir -> blocks[index]] = true;	
 			cpm_dir -> blocks[index] = 0;			
 			index ++;
 		}
-	writeDirStruct(cpm_dir,result,buffer);
-	blockWrite(buffer,(uint8_t)0); 
-	return 0;
+		writeDirStruct(cpm_dir,result,buffer);
+		blockWrite(buffer,(uint8_t)0); 
+		return 0;
 	
 	}
-	else{
+	else
+	{
 		return -1;
 	}
 }
 
 /*rename a filename with the old filename and new filename passed as parameters*/
-int cpmRename(char *old_Name, char * new_Name){
+int cpmRename(char *old_Name, char * new_Name)
+{
 	uint8_t buffer[1024];
 
 	int i=0;
@@ -333,29 +390,37 @@ int cpmRename(char *old_Name, char * new_Name){
 	DirStructType *cpm_dir;
 	blockRead(buffer,(uint8_t)0);
 	check = checkLegalName(new_Name);
-	if (check == false){
+	if (check == false)
+	{
 		return -2;
 	}
 	result= findExtentWithName(old_Name,buffer);
-	if (result!=-1){
+	if (result!=-1)
+	{
 		cpm_dir = mkDirStruct(result,buffer);
-		while(i>=0 && i<length){
-			if (new_Name[i]!='.'){
+		while(i>=0 && i<length)
+		{
+			if (new_Name[i]!='.')
+			{
 				cpm_dir -> name[j] = new_Name[i];				
 				i++;
 				j++;
 			}
-			else if (new_Name[i] == '.'){
+			else if (new_Name[i] == '.')
+			{
 				i++;
 				cpm_dir -> name[j]='\0';
 				j=0;
-				while(i>0 && i<length){
-					if (new_Name[i]!=' '){
+				while(i>0 && i<length)
+				{
+					if (new_Name[i]!=' ')
+					{
 						cpm_dir -> extension[j] = new_Name[i];
 						j++;
 						i++;
 					}
-					else if (new_Name[i]==' '){
+					else if (new_Name[i]==' ')
+					{
 						cpm_dir -> extension[j]='\0';
 						break;
 					}
@@ -364,9 +429,9 @@ int cpmRename(char *old_Name, char * new_Name){
 				cpm_dir -> extension[j]='\0';
 			}
 		}
-	writeDirStruct(cpm_dir,result,buffer);	
-	blockWrite(buffer,(uint8_t)0); 
-	return 0;
+		writeDirStruct(cpm_dir,result,buffer);	
+		blockWrite(buffer,(uint8_t)0); 
+		return 0;
 	}
 	else 
 		return -1;
